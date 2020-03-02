9ecd94e86a1c1df01ceb65528a618a527d55cb17
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2020-03-01 23:18:41 -0300

    melhorando a acessibilidade na modal

--- js/dialog.js
+++ js/dialog.js
@@ -1,19 +1,37 @@
 // Variáveis
+var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
 var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
+var videoPlayer = document.querySelector('video').controls;
 
+btnAbreDialog.style.display = 'block';
 
 // Quando abrir a dialog...
-btnAbreDialog.addEventListener('click', function() {
+btnAbreDialog.addEventListener('click', function () {
   dialog.classList.add('dialogNewsletter--aberto');
+  document.querySelector('.dialogNewsletter-campo').focus();
+  conteudoForaDialog.inert = true;
+  document.querySelector('video').controls = false;
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+  dialog.classList.remove('dialogNewsletter--aberto');
+  conteudoForaDialog.inert = false;
+  btnAbreDialog.focus();
+  document.querySelector('video').controls = true;
+
 }
 
 // Listeners
 document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
+
+dialogOverlay.addEventListener('click', fechandoDialog());
+
+document.addEventListener('keyup', function (evento) {
+  if (evento.keyCode == 27) {
+    fechandoDialog();
+  }
+});
